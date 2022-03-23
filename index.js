f464dde2b249358aade76833d19cdf5fca2cc5d9
melany14 <melany14@gmail.com>
2022-03-22 21:07:16 -0400

    changes to generate markdown

--- index.js
+++ index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your name.");
+                return false;
+            }
+        }
+    }, 
+    {    
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Describe your project in a short paragraph.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "List your installation instructions.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What is your usage information?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What are your contribution guidelines.",
+        name: "contributionGuidelines"
+    },
+    {
+        type: "input",
+        message: "Describe your test instructions.",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "What license do you need for your project?",
+        name: "license",
+        choices: [
+            "MIT",
+            "Apache",
+            "MPL",
+            "GNU",
+            "BSD",
+            "CC-BY",
+            "Unlicense"
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions().then(answers => 
+        {
+            console.log(answers);
+            writeToFile('./createdReadmeFile/README.md', generateMarkdown(answers));
+        });
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "Apache": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  "MPL": "[!"[License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  "GNU": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  "BSD": "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+  "CC-BY": "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)",
+  "Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +21,35 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installations](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributionGuidelines)
+  * [Test](#test)
+  * [Github Username](#username)
+  * [License](#license)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributions
+  ${data.contributionGuidelines}
+  ## Test
+  ${data.test}
+  ##Github Username
+  ${data.username}
+  ## License
+  ${data.license}
+  This repository follows an ${data.license} . 
+  ## Questions
+  If you have any questions, please contact me at ${data.email}
 
 `;
 }
+
+
 
 module.exports = generateMarkdown;
